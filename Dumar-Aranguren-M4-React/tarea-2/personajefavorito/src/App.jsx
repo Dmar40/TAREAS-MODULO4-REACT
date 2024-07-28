526b577ca7c1a4bbd7c20f81235f989b8ed86f06
Dmar40 <dumararanguren@gmail.com>
2024-07-28 01:54:39 -0500

    subo tarea 2 personaje favorito

---
+++ Dumar-Aranguren-M4-React/tarea-2/personajefavorito/src/App.jsx
@@ -0,0 +1,19 @@
+import  './App.css'
+
+import { Imagen } from './components/Imagen/imagenComponent';
+
+function App() {
+  return (
+    <div>
+      <h1>DRAGON BALL-GOKU</h1>
+      <Imagen/>
+      
+      <p>
+      'Son Goku, también conocido simplemente como Goku, es el protagonista principal de la serie de manga y anime Dragon Ball. Creado por Akira Toriyama, Goku es un guerrero Saiyan con habilidades sobrehumanas y un gran corazón.'
+      </p>
+      
+    </div>
+  );
+}
+
+export default App
